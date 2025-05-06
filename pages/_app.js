8f7d15f8de85959a700944bc01b313feac66a317
Ian Choo <chookaijin@gmail.com>
2025-05-06 13:58:11 +0800

    Your message here

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+// pages/_app.js
+import { useMemo } from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {  // Removed AppProps type
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter()],
+    [network]
+  );
+
+  return (
+    <WalletProvider wallets={wallets} autoConnect endpoint={endpoint}>
+      <WalletModalProvider>
+        <Component {...pageProps} />
+      </WalletModalProvider>
+    </WalletProvider>
+  );
 }
